8f8a5ff838d3c2e4144cf7a0e7deb99602884217
林宏任 <linhongren@yuexiu-finance.com>
2025-03-17 17:48:53 +0800

    提交到本地仓库

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,64 @@
+
+
+import java.lang.*;
+import java.util.*;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+         Lock lock = new ReentrantLock();
+
+        lock.lock();
+
+
+
+
+
     }
+
+
+    private int max = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        seed(root);
+        return max;
+    }
+
+    private int seed(TreeNode root) {
+        if(root == null){
+            return 0;
+        }
+        int rightLength = seed(root.right);
+        int leftLength = seed(root.left);
+
+        int value = rightLength + leftLength + 1;
+        if(max < value){
+            max = value;
+        }
+
+
+        return Math.max(rightLength, leftLength) + 1;
+    }
+
+
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+  }
+
+
+
 }
+
--- src/bloomFilter/BloomFilter.java
+++ src/bloomFilter/BloomFilter.java
@@ -1,5 +1,7 @@
 package bloomFilter;
 
+import java.util.BitSet;
+
 /**
  * @program: small-experiment
  * @description:
@@ -7,5 +9,94 @@ package bloomFilter;
  * @created: 2025/01/23 10:25
  */
 public class BloomFilter {
+    /**
+     * 位数组的大小
+     */
+    private static final int DEFAULT_SIZE = 2 << 24;
+    /**
+     * 通过这个数组可以创建 6 个不同的哈希函数
+     */
+    private static final int[] SEEDS = new int[]{3, 13, 46, 71, 91, 134};
+
+    /**
+     * 位数组。数组中的元素只能是 0 或者 1
+     */
+    private BitSet bits = new BitSet(DEFAULT_SIZE);
+
+    /**
+     * 存放包含 hash 函数的类的数组
+     */
+    private SimpleHash[] func = new SimpleHash[SEEDS.length];
+
+    /**
+     * 初始化多个包含 hash 函数的类的数组，每个类中的 hash 函数都不一样
+     */
+    public BloomFilter() {
+        // 初始化多个不同的 Hash 函数
+        for (int i = 0; i < SEEDS.length; i++) {
+            func[i] = new SimpleHash(DEFAULT_SIZE, SEEDS[i]);
+        }
+    }
+
+    /**
+     * 添加元素到位数组
+     */
+    public void add(Object value) {
+        for (SimpleHash f : func) {
+            bits.set(f.hash(value), true);
+        }
+    }
+
+    /**
+     * 判断指定元素是否存在于位数组
+     */
+    public boolean contains(Object value) {
+        boolean ret = true;
+        for (SimpleHash f : func) {
+            ret = ret && bits.get(f.hash(value));
+        }
+        return ret;
+    }
+
+    /**
+     * 静态内部类。用于 hash 操作！
+     */
+    public static class SimpleHash {
+
+        private int cap;
+        private int seed;
+
+        public SimpleHash(int cap, int seed) {
+            this.cap = cap;
+            this.seed = seed;
+        }
+
+        /**
+         * 计算 hash 值
+         */
+        public int hash(Object value) {
+            int h;
+            //h ^ (h >>> 16)：将 h 的高 16 位与低 16 位进行异或操作。这一步的目的是减少哈希冲突，使哈希值分布更加均匀。
+            //seed 的目的是增加哈希值的随机性，使哈希值分布更加均匀
+            //按位与操作确保结果在 0 到 cap - 1 之间
+            //最大cap-1也就是24位，不进行h >>> 16的话也就是24-32的位数不会参与计算，所以这样可以保证进一步散列（而进行^异或是因为异或概率为1/2，相同置0，不同置1）
+            return (value == null) ? 0 : Math.abs((cap - 1) & seed * ((h = value.hashCode()) ^ (h >>> 16)));
+        }
+
+    }
+
+    public static void main(String[] args) {
+        String value1 = "https://javaguide.cn/";
+        String value2 = "https://github.com/Snailclimb";
+        BloomFilter filter = new BloomFilter();
+        System.out.println(filter.contains(value1));
+        System.out.println(filter.contains(value2));
+        filter.add(value1);
+        filter.add(value2);
+        System.out.println(filter.contains(value1));
+        System.out.println(filter.contains(value2));
+    }
+
+
 
 }
